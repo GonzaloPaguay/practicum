8d09de074894e1528aa8a344982c6629c366f095
GonzaloPaguay <gonzalopaguay@hotmail.com>
2025-01-07 05:06:09 -0500

    Creacion MVC Pacientes y Doctores

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,11 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de Usuarios</title>
-</head>
-<body>
-    <h1>Hola mundo</h1>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+  </head>
+
+
+
+<body>   
+    <h1>Registro de Usuarios</h1>
+    
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+
+    <div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Nombres:</span>
+  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Apellidos:</span>
+  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Mail:</span>
+  <input type="text" class="form-control" placeholder="" aria-label="">
+  <span class="input-group-text">@</span>
+  <input type="text" class="form-control" placeholder="dominio" aria-label="Server">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Password:</span>
+  <input type="password" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+<button type="button" class="btn btn-success">Enviar</button>
+   
+
 </body>
 </html>
